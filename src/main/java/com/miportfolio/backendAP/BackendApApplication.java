d1c80187439bb2918035fbede85b12e0f21b7741
belenmarconi <belenmarconi@gmail.com>
2023-01-15 21:49:16 -0300

    base de datos nueva

--- src/main/java/com/miportfolio/backendAP/BackendApApplication.java
+++ src/main/java/com/miportfolio/backendAP/BackendApApplication.java
@@ -3,7 +3,6 @@ package com.miportfolio.backendAP;
 import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -20,7 +19,7 @@ public class BackendApApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontend-argprograma-bmarconi.web.app"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontend-argprograma-bmarconi.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
