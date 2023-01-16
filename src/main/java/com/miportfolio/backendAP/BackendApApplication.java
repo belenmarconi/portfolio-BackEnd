1449168bcfdc43ba4f9d649c7a28bd1d788b01a0
belenmarconi <belenmarconi@gmail.com>
2023-01-15 22:48:09 -0300

    base de datos nueva + actualizacion cors

--- src/main/java/com/miportfolio/backendAP/BackendApApplication.java
+++ src/main/java/com/miportfolio/backendAP/BackendApApplication.java
@@ -20,7 +20,7 @@ public class BackendApApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontend-argprograma-bmarconi.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontend-argprograma-bmarconi.web.app"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
