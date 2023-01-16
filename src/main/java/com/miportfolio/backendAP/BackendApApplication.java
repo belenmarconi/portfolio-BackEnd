e0143c767aa45961f78dc3e9a6928b4cdfea1934
belenmarconi <belenmarconi@gmail.com>
2023-01-15 22:35:48 -0300

    base de datos nueva-reparado

--- src/main/java/com/miportfolio/backendAP/BackendApApplication.java
+++ src/main/java/com/miportfolio/backendAP/BackendApApplication.java
@@ -3,6 +3,7 @@ package com.miportfolio.backendAP;
 import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
